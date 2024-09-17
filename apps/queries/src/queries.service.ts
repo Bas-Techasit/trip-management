da1cc264941bb948d99929a586bc0dc8bea748b0
bas <techasit.bas@gmail.com>
2024-09-17 17:23:09 +0700

    Hello, World

--- apps/queries/src/queries.service.ts
+++ apps/queries/src/queries.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class QueriesService {
   getHello(): string {
-    return 'Test changed this file to test ci/cd!';
+    return 'Hello, World!';
   }
 }
